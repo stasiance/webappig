ca866b475463a7d6564a387a17cf80f4e7bdf370
gaoasiance <gaofeng@asiance.com>
2016-11-15 15:22:50 +0900

    Front first version

--- assets/inc/header.php
+++ assets/inc/header.php
@@ -9,15 +9,18 @@
     <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
     <meta name="description" content="">
 
-    <meta name="author" content="">
+    <meta name="author" content="Asiance">
+	
+	<link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Questrial" rel="stylesheet">
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" type="text/css">
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" type="text/css">
-    <link rel="stylesheet" href="/assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/main.css">
 
 
     <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
     <script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="/asset/js/main.js"></script>
+    <script type="text/javascript" src="asset/js/main.js"></script>
 </head>
 <body>
--- index.php
+++ index.php
@@ -2,7 +2,38 @@
 include 'assets/inc/header.php';
 ?>
 
-Hello !
+<header id="header" class="clearfix">
+	<div class="inner-wrapper">
+		<a href="/" class="site-title">
+			Hashure
+		</a>
+
+		<div id="search">
+			<form action="" id="search-form">
+				<span class="input">
+					<input id="search-input" type="text" class="input-text" placeholder="Search">
+				</span>
+			</form>
+		</div>
+	</div>
+</header>
+
+<section id="main">
+	<div id="results">
+	
+	</div>
+</section>
+
+<footer id="footer">
+	<div class="inner-wrapper clearfix">
+		<span class="left">
+			Copyright Microsoft Azure 2016
+		</span>
+		<span class="right">
+			Made by Asiance
+		</span>
+	</div>
+</footer>
 
 <?php
 include 'assets/inc/footer.php';
