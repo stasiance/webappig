68ac1dd2df0bf5cf5e8fbf339887201ef38d5084
gaoasiance <gaofeng@asiance.com>
2016-11-15 16:53:19 +0900

    Search bar and dummy results

--- index.php
+++ index.php
@@ -9,10 +9,66 @@ include 'assets/inc/header.php';
 		</a>
 
 		<div id="search">
-			<form action="" id="search-form">
+			<form action="" id="search-form" class="searching">
 				<span class="input">
 					<input id="search-input" type="text" class="input-text" placeholder="Search">
 				</span>
+				
+				<div class="results-preview show">
+					<span class="triangles">
+						<span class="triangle-1"></span>
+						<span class="triangle-2"></span>
+					</span>
+					<div class="results">
+						<ul class="list">
+							<li class="list-item">
+								<a href="" class="result-link">
+									<span class="type">
+										#
+									</span>
+									<span class="info">
+										<span class="nickname">
+											korea
+										</span>
+										<span class="figure">
+											19,200,000 posts
+										</span>
+									</span>
+								</a>
+							</li>
+							<li class="list-item">
+								<a href="" class="result-link">
+									<span class="type">
+										@
+									</span>
+									<span class="info">
+										<span class="nickname">
+											goalkorea
+										</span>
+										<span class="figure">
+											19,200,000 posts
+										</span>
+									</span>
+								</a>
+							</li>
+							<li class="list-item">
+								<a href="" class="result-link">
+									<span class="type">
+										#
+									</span>
+									<span class="info">
+										<span class="nickname">
+											peopleskorea
+										</span>
+										<span class="figure">
+											19,200,000 posts
+										</span>
+									</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
 			</form>
 		</div>
 	</div>
@@ -20,7 +76,122 @@ include 'assets/inc/header.php';
 
 <section id="main">
 	<div id="results">
-	
+		<ul class="list">
+			<li class="list-item">
+				<div class="top">
+					<div class="nickname">
+						<a href="" class="user-link" target="_blank">
+							blablabla
+						</a>
+					</div>
+					<div class="title">
+						<a href="" class="place-link" target="_blank">
+							In Miami
+						</a>
+					</div>
+				</div>
+				<div class="image">
+					<a href="" class="post-link" target="_blank">
+						<img src="http://www.naeem.co.za/wp/wp-content/uploads/2012/12/instagram-digest-20121207-4.jpg" alt="">
+					</a>
+				</div>
+			</li>
+			<li class="list-item">
+				<div class="top">
+					<div class="nickname">
+						<a href="" class="user-link" target="_blank">
+							blablabla
+						</a>
+					</div>
+					<div class="title">
+						<a href="" class="place-link" target="_blank">
+							In Miami
+						</a>
+					</div>
+				</div>
+				<div class="image">
+					<a href="" class="post-link" target="_blank">
+						<img src="http://67.media.tumblr.com/tumblr_mdkdyxfWMO1qmvufyo1_400.jpg" alt="">
+					</a>
+				</div>
+			</li>
+			<li class="list-item">
+				<div class="top">
+					<div class="nickname">
+						<a href="" class="user-link" target="_blank">
+							blablabla
+						</a>
+					</div>
+					<div class="title">
+						<a href="" class="place-link" target="_blank">
+							In Miami
+						</a>
+					</div>
+				</div>
+				<div class="image">
+					<a href="" class="post-link" target="_blank">
+						<img src="http://data.whicdn.com/images/67015387/large.jpg" alt="">
+					</a>
+				</div>
+			</li>
+			<li class="list-item">
+				<div class="top">
+					<div class="nickname">
+						<a href="" class="user-link" target="_blank">
+							blablabla
+						</a>
+					</div>
+					<div class="title">
+						<a href="" class="place-link" target="_blank">
+							In Miami
+						</a>
+					</div>
+				</div>
+				<div class="image">
+					<a href="" class="post-link" target="_blank">
+						<img src="http://www.savvysassymoms.com/blog/wp-content/uploads/2014/04/IMG_3711.jpg" alt="">
+					</a>
+				</div>
+			</li>
+			<li class="list-item">
+				<div class="top">
+					<div class="nickname">
+						<a href="" class="user-link" target="_blank">
+							blablabla
+						</a>
+					</div>
+					<div class="title">
+						<a href="" class="place-link" target="_blank">
+							In Miami
+						</a>
+					</div>
+				</div>
+				<div class="image">
+					<a href="" class="post-link" target="_blank">
+						<img src="http://www.savvysassymoms.com/blog/wp-content/uploads/2014/04/IMG_3711.jpg" alt="">
+					</a>
+				</div>
+			</li>
+			<li class="list-item">
+				<div class="top">
+					<div class="nickname">
+						<a href="" class="user-link" target="_blank">
+							blablabla
+						</a>
+					</div>
+					<div class="title">
+						<a href="" class="place-link" target="_blank">
+							In Miami
+						</a>
+					</div>
+				</div>
+				<div class="image">
+					<a href="" class="post-link" target="_blank">
+						<img src="http://data.whicdn.com/images/67015387/large.jpg" alt="">
+					</a>
+				</div>
+			</li>
+		</ul>
 	</div>
 </section>
 
