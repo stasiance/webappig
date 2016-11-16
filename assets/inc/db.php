831694db728b65eff37c8134a87ac0764683db04
Senthilraja <senthilraja@asiance.com>
2016-11-16 16:51:56 +0900

    db connection string updated

--- assets/inc/db.php
+++ assets/inc/db.php
@@ -6,11 +6,12 @@ function go_mysql($query) {
         $host = $_SERVER['HTTP_HOST'];
         if($host === 'localhost') {
             $db = @mysql_connect("localhost", "root", "") or die(mysql_error());
+            @mysql_select_db("appig") or die(mysql_error());
         }
         else {
-            $db = @mysql_connect("ap-cdbr-azure-southeast-b.cloudapp.net", "appigdb", "asiance:2016") or die(mysql_error());
+            $db = @mysql_connect("ap-cdbr-azure-southeast-b.cloudapp.net", "be7a756e526e2a", "eb0e82fa") or die(mysql_error());
+            @mysql_select_db("database_appig") or die(mysql_error());
         }
-        @mysql_select_db("appig") or die(mysql_error());
         @mysql_query("SET NAMES 'utf8'");
         @mysql_query("set character_set_client='utf8'");
         @mysql_query("set character_set_results='utf8'");
