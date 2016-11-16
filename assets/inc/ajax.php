c5171706f665dcba3eec49852cc1646ad39332f2
Senthilraja <senthilraja@asiance.com>
2016-11-16 13:55:46 +0900

    conflict fix

--- assets/inc/ajax.php
+++ assets/inc/ajax.php
@@ -23,12 +23,6 @@ switch ($_REQUEST['m']) {
         echo $result;
         break;
 	
-	case 'searchUser':
-		$url = 'https://api.instagram.com/v1/users/' . $_REQUEST['q'] . '/media/recent/?access_token=' . $access_token;
-		$result = file_get_contents($url);
-		echo $result;
-		break;
-	
 	case 'searchLocation':
 		$url = 'https://api.instagram.com/v1/locations/' . $_REQUEST['q'] . '/media/recent/?access_token=' . $access_token;
 		$result = file_get_contents($url);
